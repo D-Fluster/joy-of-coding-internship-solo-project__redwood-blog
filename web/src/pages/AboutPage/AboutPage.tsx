8cbfe2b1bb4a4a8d552c39556d2632ec77901fa7
D-Fluster <158021342+D-Fluster@users.noreply.github.com>
2024-03-13 22:10:55 -0700

    Tutorial chapters 0 & 1 complete

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,26 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Metadata title="About" description="About page" />
+      <main>
+      <h1>AboutPage</h1>
+      <p>
+        Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
+      </p>
+      <p>
+        My default route is named <code>about</code>, link to me with `
+        <Link to={routes.about()}>About</Link>`
+      </p> <p>
+          This site was created to demonstrate my mastery of Redwood: Look on my
+          works, ye mighty, and despair!
+        </p>
+      </main>
+
+    </>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,23 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+
+<main>
+      <h1>Wazzaaap, I'm the Home Page&nbsp;<em><strong>!</strong></em></h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
+      </p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+      </main>
+    </>
+  )
+}
+
+export default HomePage
