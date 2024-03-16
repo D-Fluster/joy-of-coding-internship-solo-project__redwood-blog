a51027d53f3e5a76cdf3bc8a0c437ff30edc90b6
D-Fluster <158021342+D-Fluster@users.noreply.github.com>
2024-03-16 15:12:30 -0700

    Test commit for Netlify redeployment

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -4,22 +4,24 @@ import { Metadata } from '@redwoodjs/web'
 const AboutPage = () => {
   return (
     <>
-      <Metadata title="About" description="About page" />
+      <Metadata title="About" description="About Us" />
       <main>
-      <h4><em>(~ BOILERPLATE ~)</em></h4>
-      <h1>AboutPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>about</code>, link to me with `
-        <Link to={routes.about()}>About</Link>`
-      </p> <p>
+        <h4>
+          <em>(~ BOILERPLATE+ ~)</em>
+        </h4>
+        <h1>About Page</h1>
+        <p>
+          Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
+        </p>
+        <p>
+          My default route is named <code>about</code>, link to me with `
+          <Link to={routes.about()}>About</Link>`
+        </p>{' '}
+        <p>
           This site was created to demonstrate my mastery of Redwood: Look on my
           works, ye mighty, and despair!
         </p>
       </main>
-
     </>
   )
 }
